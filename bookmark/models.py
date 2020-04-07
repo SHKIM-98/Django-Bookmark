021197a7979d2965e3e2f8fe6f0dc25c3c7466c9
shkim <cubix4549@gmail.com>
2020-04-07 03:12:25 -0400

    Bookmark-Project

---
+++ bookmark/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Bookmark(models.Model):
+    site_name = models.CharField(max_length = 50)
+    url = models.CharField(max_length = 1000)
+
+
+# Create your models here.
